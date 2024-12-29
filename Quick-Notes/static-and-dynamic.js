04e23016f75e2e4374486817403a7f9ed72eedf1
Pratik Thorat <thorat.p@northeastern.edu>
2024-12-29 10:55:22 -0500

    changes

--- Quick-Notes/static-and-dynamic.js
+++
@@ -1,4 +0,0 @@
-// Static Arrays vs Dynamic arrays
-// Static arrays have limitations such as fixed size
-// Dynamic arrays solve this problem by allocating a copy of it all into memory.
-// Arrays in Javascript are by default dynamic.
