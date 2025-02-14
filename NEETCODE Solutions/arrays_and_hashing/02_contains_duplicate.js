457e4a245b5c98d17ac7ac099406cad5b870055f
Pratik Thorat <thorat.p@northeastern.edu>
2025-02-13 22:10:04 -0500

    contains duplicates all solutions

--- NEETCODE Solutions/arrays_and_hashing/02_contains_duplicate.js
+++ NEETCODE Solutions/arrays_and_hashing/02_contains_duplicate.js
@@ -1,23 +1,51 @@
 /**
  * @param {number[]} nums
  * @return {boolean}
-Brute Force. Time Complexity: O(n^2) O(1) 
-let containsDuplicate = function (nums) {
-  // Handling the case where array is empty.
-  if (nums.length < 1) {
-    return false;
-  }
-
-  // Checking duplicates.
+ */
+var containsDuplicate = function (nums) {
+  // Brute Force
+  /*
   for (let i = 0; i < nums.length; i++) {
-    let j = i + 1; //updates by i+1 to go into the next index.
-    while (j < nums.length) {
-      if (nums[i] === nums[j]) {
-        return true;
+          for (let j = i + 1; j < nums.length; j++) {
+              if (nums[i] === nums[j]) {
+                  return true;
+              }
+          }
       }
-      j++;
-    }
-  }
+      return false;
+      Time: O(N^2)
+      Space: O(1)
+  */
+  // Sorting method
+  /*
+  nums.sort((a, b) => a - b);
+      for (let i = 1; i < nums.length; i++) {
+          if (nums[i] === nums[i - 1]) {
+              return true;
+          }
+      }
   return false;
+  Time: O(nlogn)
+  Space: O(n) or O(1) depending on the algorithm
+  */
+  // Optimal Solution:
+  /*
+  const seen = new Set();
+      for (const num of nums) {
+          if (seen.has(num)) {
+              return true;
+          }
+          seen.add(num);
+      }
+      return false;
+      Time: O(N)
+      Space: O(N)
+  */
+  // Optimal Solution
+  /*
+  const seen = new Set(nums);
+  return seen.size < nums.length;
+  Time: O(N)
+  Space: O(N)
+  */
 };
-*/
